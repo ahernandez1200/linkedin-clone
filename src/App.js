0a518b527557982e653c30f00a039fc2452401c6
ahernandez1200 <tony.6.h@gmail.com>
2021-07-28 02:48:00 -0700

    earlier work

--- src/App.js
+++ src/App.js
@@ -1,58 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+
 import './App.css';
+import Header from './Header.js';
+import Sidebar from './Sidebar.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="app">
+      <Header/>
+
+      {/* App Body */}
+      <div className="app__body">
+        {/* Sidebar */}
+        <Sidebar/>
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
+     
     </div>
   );
 }
 
 export default App;
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCpUq8bsl0VBYDKxTcfN6i4ltQUwYtEOrk",
+//   authDomain: "linkedin-clone-1e7e2.firebaseapp.com",
+//   projectId: "linkedin-clone-1e7e2",
+//   storageBucket: "linkedin-clone-1e7e2.appspot.com",
+//   messagingSenderId: "618893599950",
+//   appId: "1:618893599950:web:f33072637d09ef219fdc9f"
+// };
