2fddb0c642bfc7e9bdf2e3de5da324165eba1e3d
ahernandez1200 <tony.6.h@gmail.com>
2021-07-31 02:35:37 -0700

    build Input section

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import "./Feed.css"
 import CreateIcon from "@material-ui/icons/Create"
+import ImageIcon from "@material-ui/icons/Image"
+import SubscriptionIcon from "@material-ui/icons/Subscriptions"
+import EventNoteIcon from "@material-ui/icons/EventNote"
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay"
+
+import InputOption from './InputOption';
+
 
 function Feed() {
     return (
@@ -13,8 +20,17 @@ function Feed() {
                         <button type="submit">send</button>
                     </form>
                 </div>
+                <div className="feed_inputOptions">
+                    <InputOption title="Photo" Icon={ImageIcon} color="#7005f9"/>
+                    <InputOption title="Video" Icon={SubscriptionIcon} color="#C7A33E"/>
+                    <InputOption title="Event" Icon={EventNoteIcon} color="#C0CBCD"/>
+                    <InputOption title="Write Article" Icon={CalendarViewDayIcon} 
+                        color="#7FC15E"/>
+
+                </div>
 
             </div>
+            {/* posts */}
         </div>
     )
 }
---
+++ src/Post.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Post.css"
+
+
+function Post({name, description, message, photoURL}) {
+    return (
+        <div className="post">
+            <div className="post__header">
+                <Avatar/>
+                <div className="post__info">
+                    <h2>Anthony Hernandez</h2>
+                    <p>Description</p>
+                </div>
+            </div>
+            
+            <div className="post__body">
+                <p>message goes here</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Post;
+
