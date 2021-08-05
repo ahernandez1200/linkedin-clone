9b524ec39c134fcfc791078a095feb48979d0c21
ahernandez1200 <tony.6.h@gmail.com>
2021-08-04 20:41:03 -0700

    integrate redux with application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header.js';
+import Login from './Login';
 import Sidebar from './Sidebar.js'
 
 function App() {
+
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header/>
-
-      {/* App Body */}
-      <div className="app__body">
-        {/* Sidebar */}
-        <Sidebar/>
-        {/* Feed */}
-        <Feed/>
-        {/* Widgets */}
-      </div>
+      {!user ? <Login /> : (
+        <div className="app__body">
+          <Sidebar/>
+          <Feed/>
+          {/* Widgets */}
+        </div>
+      )}
+     
      
     </div>
   );
